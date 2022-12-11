6b907d2610a3ed10fffb336ee826e390dab41e48
manong <aa1348769445@gmail.com>
2022-12-11 23:38:38 +0800

    提交

--- lib/baidu_map_view.dart
+++ lib/baidu_map_view.dart
@@ -19,15 +19,76 @@ class _BaiduMapViewState extends State<BaiduMapView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        child: BMFMapWidget(
-          onBMFMapCreated: (controller) {
-            // onBMFMapCreated(controller);
-          },
-          mapOptions: mapOptions,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          "位置",
+          style: TextStyle(color: Colors.grey.shade800, fontSize: 16),
         ),
+        elevation: 0,
+        backgroundColor: Colors.white,
+        toolbarHeight: 45,
+        leadingWidth: 35,
+        leading: Icon(
+          Icons.arrow_back_ios_new_rounded,
+          color: Colors.grey.shade800,
+          size: 18,
+        ),
+        actions: [
+          Container(
+            width: 50,
+            alignment: Alignment.center,
+            child: Text(
+              "选择",
+              style: TextStyle(color: Colors.grey.shade800, fontSize: 16),
+            ),
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height * 0.25,
+            width: MediaQuery.of(context).size.width,
+            child: BMFMapWidget(
+              onBMFMapCreated: (controller) {
+                // onBMFMapCreated(controller);
+              },
+              mapOptions: mapOptions,
+            ),
+          ),
+          Container(
+            height: 60,
+            padding: const EdgeInsets.symmetric(vertical: 10,horizontal: 15),
+            child: ClipRRect(child: TextField(
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                filled: true,
+                fillColor: Colors.grey.shade200,
+                hintText: "搜索",
+                prefixIcon: Icon(Icons.search_rounded,size: 20,),
+                suffixIcon: MaterialButton(onPressed: (){},color: Colors.grey.shade300,child: Text("搜索"),)
+              ),
+            ),borderRadius: BorderRadius.circular(5),)
+          ),
+          Expanded(
+              child: ListView.separated(itemBuilder: (BuildContext context, int index) { 
+                return ListTile(
+                  title: Text(
+                    "哈哈哈哈",
+                    style: TextStyle(fontSize: 15),
+                  ),
+                  subtitle: Text(
+                    "asdasdasdasdasdasdas",
+                    style: TextStyle(fontSize: 13),
+                  ),
+                );
+              }, separatorBuilder: (BuildContext context, int index) { 
+                return Divider(height: 0,endIndent: 15,indent:15,);
+              }, itemCount: 50,
+           
+          ))
+        ],
       ),
     );
   }
